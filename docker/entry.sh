65d86f9e6c9a06c38261c9e4d781341b3dafe961
codeparce <codeparcelts@gmail.com>
2026-06-06 20:06:34 -0500

    Devops : new : deployment y escalado horizontal

---
+++ docker/entry.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "Starting GitHub Runner configuration and execution..."
+echo "URL_ORGANIZATION: ${URL_ORGANIZATION}"
+
+# Install dependencies
+sudo ./bin/installdependencies.sh
+
+# Create the runner and start the configuration experience
+echo "Configuring the GitHub Runner with the provided URL and token..."
+./config.sh --url  ${URL_ORGANIZATION} --token ${RUNNER_TOKEN} \
+  --name "runner-devops-01" \
+  --runnergroup "hosted" \
+  --labels "devops,k8s,docker" \
+  --work "_work" \
+  --unattended
+
+#Last step, run it!
+echo "Running the GitHub Runner..."
+./run.sh
